21a69a8a5400ed056b7e34856136df5bdc3f06f8
Juan Gallostra <juangallostra@gmail.com>
2020-03-22 13:09:39 +0100

    Add turn based option

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ from renderer import RenderScreen
 FONT_PATH = 'arial10x10.png'
 FONT_FLAGS = tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD
 
-def main():
-    # instantiate player
+def get_key_event(turn_based=None):
+    if turn_based:
+        # Turn-based game play; wait for a key stroke
+        key = tcod.console_wait_for_keypress(True)
+    else:
+        # Real-time game play; don't wait for a player's key stroke
+        key = tcod.console_check_for_keypress()
+    return key
+
+def main(turn_based):
     renderer = RenderScreen()
+    # instantiate player
     player = MainPlayer(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, '@')
     # Game loop
     while not tcod.console_is_window_closed():
-        key = tcod.console_check_for_keypress()
+        key = get_key_event(turn_based)
         if key.vk == tcod.KEY_ENTER and key.lalt:
             #Alt+Enter: toggle fullscreen
             tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
@@ -24,7 +33,8 @@ def main():
 
 if __name__ == "__main__":
     # Game config
+    turn_based = False
     tcod.console_set_custom_font(FONT_PATH, FONT_FLAGS)
     tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE, FULLSCREEN)
     tcod.sys_set_fps(LIMIT_FPS)
-    main()
+    main(turn_based)
